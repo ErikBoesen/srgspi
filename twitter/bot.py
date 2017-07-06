7302628fbe494000b1d8883247891d0998bbc6bb
ErikBoesen <me@erikboesen.com>
2017-07-06 10:10:05 -0400

    Catch repeated tweets without crashing

--- twitter/bot.py
+++ twitter/bot.py
@@ -2,7 +2,7 @@ import tweepy
 import time
 import sys
 import yaml
-import subprocess
+from subprocess import check_output
 
 with open('options.yaml') as yf:
     OP = yaml.load(yf.read())
@@ -12,9 +12,15 @@ auth.set_access_token(OP['access-key'], OP['access-secret'])
 t = tweepy.API(auth)
 
 while True:
-    disk_usage = subprocess.call('df -Bm | grep /dev/root | awk \'{print substr($3, 1, length($3)-1)"/"substr($2, 1, length($2)-1)}\'', shell=True).decode().strip()
-    t.update_status('I\'m currently using {} blocks on my SD card.'.format(disk_usage))
+    disk_usage = check_output('df -Bm | grep /dev/root | awk \'{print substr($3, 1, length($3)-1)"/"substr($2, 1, length($2)-1)}\'', shell=True).decode().strip()
+    try:
+        t.update_status('I\'m currently using {} blocks on my SD card.'.format(disk_usage))
+    except tweepy.error.TweepError: # Duplicate status, usually
+        print('Disk usage has not changed, skipping tweet.')
     time.sleep(60)
-    ram_usage = subprocess.call('free -h | grep "Mem:" | awk \'{print substr($3, 1, length($3)-1)"/"substr($2, 1, length($2)-1)}\'', shell=True).decode().strip()
-    t.update_status('I\'m currently using {} MB of my system\'s RAM.'.format(ram_usage))
+    ram_usage = check_output('free -h | grep "Mem:" | awk \'{print substr($3, 1, length($3)-1)"/"substr($2, 1, length($2)-1)}\'', shell=True).decode().strip()
+    try:
+        t.update_status('I\'m currently using {} MB of my system\'s RAM.'.format(ram_usage))
+    except tweepy.error.TweepError: # Duplicate status, usually
+        print('RAM usage has already been posted, skipping tweet.')
     time.sleep(60)
